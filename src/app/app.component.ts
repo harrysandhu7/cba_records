2d8c8cc898be36351b3907b48ab3320d20004e78
Harvinder Singh <Harvinder.Singh@tattsgroup.com>
2019-01-29 08:50:51 +1100

    Enable GITHub Pages

--- src/app/app.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  template: ` <cba-views></cba-views>`,
-})
-export class AppComponent {
-}
--- src/app/in-memory-data.service.ts
+++
@@ -1,9 +0,0 @@
-import { InMemoryDbService } from 'angular-in-memory-web-api';
-import * as options from '../assets/data/options.json';
-import * as warrants from '../assets/data/warrants.json';
-
-export class InMemoryDataService implements InMemoryDbService {
-  createDb() {
-    return {options, warrants};
-  }
-}
